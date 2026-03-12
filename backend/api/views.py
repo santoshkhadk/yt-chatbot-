8a39cdd9f9f1bf09aeaa5ef83c0edb996fb17124
santoshkhadk <santoshkhadka655@gmail.com>
2026-03-12 18:31:35 +0545

    allwoing multiple urls in backend

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,24 +1,28 @@
+# views.py
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rag.rag_pipeline import answer_youtube_question 
-
-
+from rag.rag_pipeline import answer_question_multi_video
 import os
 from dotenv import load_dotenv
 
-# load .env file
 load_dotenv()
-
 API_KEY = os.getenv("GROQ_API_KEY")
+
 
 @api_view(['POST'])
 def ask_question(request):
-    video_url = request.data.get("url")
+    """
+    Accepts JSON:
+    {
+        "urls": ["https://www.youtube.com/watch?v=VIDEO_ID1", "https://www.youtube.com/watch?v=VIDEO_ID2"],
+        "question": "Your question here"
+    }
+    """
+    video_urls = request.data.get("urls", [])
     question = request.data.get("question")
 
-    # Extract YouTube video ID
-    video_id = video_url.split("v=")[-1]
+    if not video_urls or not question:
+        return Response({"error": "Video URLs and question are required"}, status=400)
 
-    answer = answer_youtube_question(video_id, question, API_KEY)
-
+    answer = answer_question_multi_video(video_urls, question, API_KEY)
     return Response({"answer": answer})
